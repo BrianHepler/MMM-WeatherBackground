13224dd5c8f3060cc45a09fdba70b464826fe5b1
John Wade <johnw8017@gmail.com>
2018-10-06 20:28:20 -0400

    Add files via upload

--- MMM-WeatherBackground.js
+++ MMM-WeatherBackground.js
@@ -26,48 +26,50 @@ Module.register("MMM-WeatherBackground", {
     notification: "WEATHER", //if you use other weather module, modify this.
 	
     payloadConverter: (payload)=> {
+		console.log('this is from weatherbackground '+JSON.stringify(payload))
 		 payload = payload.icon; 
          sunset = moment(sunset).format('HH'), 
-		//working-> console.log(sunset),
 		 now = moment().format('HH');
-		 console.log("now: "+now+ "Sunset: "+sunset);
-		  
+		 console.log("now: "+now+ " Sunset: "+sunset);
+		 console.log(payload);
 		 if (now >= sunset) {
 			  var iconMap = {
-		    "clear": "clear night",
-  			"sleet": "sleet night",
-  			"mostlycloudy": "cloudy night",
-  			"cloudy": "cloudy night",
-  			"rain": "night rain",
-  			"drizzle": "drizzle rain night",
-  			"thunderstorm": "night thunderstorm",
-  			"snow": "snow night",
-  			"windy": "windy night",
-			"fog":"fog night",
-			"overcast":"overcast night"
-	  } 
-	   console.log(iconMap[payload]);	
-	  return iconMap[payload] //return value be used for search keyword.
-			 
+		    "clear": "modules/MMM-WeatherBackground/images/n_clear.jpg",
+  			"sleet": "modules/MMM-WeatherBackground/images/n_sleet.jpg",
+  			"mostlycloudy": "modules/MMM-WeatherBackground/images/n_mostlycloudy.jpg",
+			"partlycloudy":"modules/MMM-WeatherBackground/images/n_partlycloudy.jpg",
+  			"cloudy": "modules/MMM-WeatherBackground/images/n_cloudy.jpg",
+  			"rain": "modules/MMM-WeatherBackground/images/n_rain.jpg",
+  			"drizzle": "modules/MMM-WeatherBackground/images/n_drizzle.jpg",
+  			"tstorms": "modules/MMM-WeatherBackground/images/n_tstorms.jpg",
+  			"snow": "modules/MMM-WeatherBackground/images/n_snow.jpg",
+  			"windy": "modules/MMM-WeatherBackground/images/n_windy.jpg",
+			"fog": "modules/MMM-WeatherBackground/images/n_fog.jpg",
+			"overcast": "modules/MMM-WeatherBackground/images/n_overcast.jpg",
+			"hazy": "modules/MMM-WeatherBackground/images/n_hazy.jpg"
+	  }	 
+	  return iconMap[payload]
+		 	 
 		 } else {	
 			 
       var iconMap = {
-            "cloudy": "clouds",
-  			"mostlycloudy": "clouds",
-  			"overcast": "overcast",
-  			"wind": "windy",
-  			"rain": "heavy rain",
-  			"rain": "rain",
-  			"thunderstorm": "thunderstorm",
-  			"snow": "snow",
-  			"fog": "fog",
-  			"clear": "clear",
-  			"sleet": "sleet",
-			"overcast":"overcast"
-      }
-      console.log(iconMap[payload]+"day");	  
-	  return iconMap[payload] //return value be used for search keyword.
-	  
+		    "sunny":"modules/MMM-WeatherBackground/images/clear.jpg",
+		    "clear": "modules/MMM-WeatherBackground/images/clear.jpg",
+            "cloudy": "modules/MMM-WeatherBackground/images/cloudy.jpg",
+  			"mostlycloudy": "modules/MMM-WeatherBackground/images/mostlycloudy.jpg",
+			"partlycloudy":"modules/MMM-WeatherBackground/images/partlycloudy.jpg",
+  			"overcast": "modules/MMM-WeatherBackground/images/overcast.jpg",
+  			"wind": "modules/MMM-WeatherBackground/images/windy.jpg", 
+  			"rain": "modules/MMM-WeatherBackground/images/rain.jpg",
+  			"tstorms": "modules/MMM-WeatherBackground/images/tstorms.jpg",
+  			"snow": "modules/MMM-WeatherBackground/images/snow.jpg",
+  			"fog": "modules/MMM-WeatherBackground/images/fog.jpg", 
+  			"sleet": "modules/MMM-WeatherBackground/images/sleet.jpg",
+			"overcast":"modules/MMM-WeatherBackground/images/overcast.jpg",
+			"hazy":"modules/MMM-WeatherBackground/images/hazy.jpg",
+            "drizzle": "modules/MMM-WeatherBackground/images/drizzle.jpg",			
+      }	 
+	  return iconMap[payload]
 		 }
     },
   },
@@ -78,7 +80,7 @@ Module.register("MMM-WeatherBackground", {
         break
       case this.config.notification :
         var target = (this.config.targetDOM) ? this.config.targetDOM : "#" + sender.data.identifier
-
+ 
         var t = this.config.payloadConverter(payload)
         this.loadImage(target, t)
         break
@@ -87,15 +89,11 @@ Module.register("MMM-WeatherBackground", {
 
   loadImage: function(target, description) {
     var drawImage = function(dom) {
-      var timer = setTimeout(()=>{
-        //var w = dom.offsetWidth
-        //var h = dom.offsetHeight
-        //var url = `https://source.unsplash.com/${w}x${h}/?"${description}"`
-        var seed = Date.now()
-        var url = `https://source.unsplash.com/featured/?"${description}"&s=${seed}`
-        dom.style.backgroundSize = "cover"
+      var timer = setTimeout(()=>{ 
+        var url = description
+		dom.style.backgroundSize = "cover"
         dom.style.transition = "background-image 1s ease 1s"
-        var url = `url('${url}')`
+        var url = `url('${url}')` 
         dom.style.backgroundImage = url
       }, 1000)
     }
